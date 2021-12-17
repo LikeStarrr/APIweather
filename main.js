01518c772d4ca573b4ff123465d3b643cb072227
LikeStarrr <92567777+LikeStarrr@users.noreply.github.com>
2021-12-17 16:47:44 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,55 +1,67 @@
-const UI_ELEMENTS = {
-    INPUT_SEARCH: document.querySelector(".input-search"),
-    BTN_SEARCH: document.querySelector(".btn-search"),
-    BTN_FAVORITE: document.querySelector(".btn-favorite"),
-    TAB_NOW: document.querySelector(".tab_now"),
-    TAB_DETAILS: document.querySelector(".tab_details"),
-    TAB_FORECAST: document.querySelector(".tab_forecast"),
+const UI = {
+  INPUT_SEARCH: document.querySelector(".input-search"),
+  BTN_SEARCH: document.querySelector(".btn-search"),
+  BTN_FAVORITE: document.querySelector(".btn-favorite"),
+  IMG_FAVORITE: document.querySelector(".btn-favorite"),
+  TAB_NOW: document.querySelector(".tab_now"),
+  TAB_DETAILS: document.querySelector(".tab_details"),
+  TAB_FORECAST: document.querySelector(".tab_forecast"),
+  UL_DETAILS: document.querySelectorAll(".list-elem"),
+  TEMPERATURE_NOW: document.querySelector(".temperature"),
+  IMG_WEATHER_NOW: document.querySelector(".icon-weather"),
+  LOCATION_NOW: document.querySelector(".now-location"),
+  LOCATION_DETAILS: document.querySelector(".details-location"),
+};
 
-}
+UI.BTN_SEARCH.addEventListener("click", function () {
+  getWeather();
+});
 
-UI_ELEMENTS.BTN_SEARCH.addEventListener('click', function() {    
-    getWeather(location); 
-})
+function getWeather() {
+  const serverUrl = "http://api.openweathermap.org/data/2.5/weather";
+  const cityName = UI.INPUT_SEARCH.value;
+  const apiKey = "f660a2fb1e4bad108d6160b7f58c555f";
+  const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
+  alert(url);
+  fetch(url)
+    .then(function (response) {
+      if (response.status !== 200) {
+         return Promise.reject(new Error(response.statusText))  
+      } 
+      return Promise.resolve(response)
+    })
+    .then((response) => response.json())
+    .then((result) => {
+      let weatherInfo = {
+        Temperature: Math.round(result.main.temp) + "\u2103",
+        "Feels like": Math.round(result.main.feels_like) + "\u2103",
+        Weather: result.weather[0].main,
+        Sunrise: getTimeFromUnix(result.sys.sunrise),
+        Sunset: getTimeFromUnix(result.sys.sunset),
+      };
 
-function getWeather(location) {
-    const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
-    const cityName = 'boston';
-    const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
-    const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
-    fetch(url);
-}
-
-
-
-	ADD.BTNS[i].addEventListener ('click', function () {
- 
-    let isAddContent = ADD.INPUTS[i].value.trim();
-    try{
-       if (!isAddContent) {
-         throw "err1";
-       }
-    } catch(error) {
-      if (error=="err1") {
-        alert("Enter new task!");
-        return;
+      let i = 0;
+      for (let key in weatherInfo) {
+        UI.UL_DETAILS[i].textContent = `${key} : ${weatherInfo[key]} `;
+        i++;
       }
-    }
-    const DIV = document.createElement('div');
-    DIV.className = 'task';
+      UI.TEMPERATURE_NOW.textContent = Math.round(result.main.temp) + "\u2103";
+      UI.LOCATION_NOW.textContent = cityName;
+      UI.LOCATION_DETAILS.textContent = cityName;
+    })
+    .catch(Error => alert('Error ', Error));  
+  UI.INPUT_SEARCH.value = "";
+}
 
-    DIV.innerHTML = `<label> 
-        <input class="task_check" type="checkbox"> 
-        <span class="cursor"></span>
-      </label>
-      <span class="task_text" > ${ADD.INPUTS[i].value} </span>
-      <button class="task_delete"><img src="./img/delete-task-icon.svg" alt="-"></button>`	
- 
-      PRIORITY_BLOCKS[i].append(DIV);
+function getTimeFromUnix(timestamp) {
+  let d = new Date(timestamp * 1000);
+  timeStampCon = addZero(d.getHours()) + ":" + addZero(d.getMinutes());
+  return timeStampCon;
+}
 
-    ADD.INPUTS[i].value = '';
-
-    DIV.querySelector('.task_check').addEventListener('click', changeColor);
-    DIV.querySelector('.task_delete').addEventListener('click', deleteTask); 
-  })
-
+function addZero(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
